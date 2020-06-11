cda110b145b99790e1ff7e4b4c4fa8fdd63b419b
lspss9950101 <lspss9950101@gmail.com>
2020-06-11 15:42:59 +0800

    add new conditions.

--- public/src/graduate/static/additional_condition.js
+++ public/src/graduate/static/additional_condition.js
@@ -30,3 +30,18 @@ module.exports.english_credit_blacklist = [
 	'資訊工程研討(英文授課)',
 	'資訊工程研討',
 ];
+
+module.exports.merge_exceptions = [
+	'GEC0020',//外院
+	'GEC0019',
+	'GEC0011',//人文
+	'GEC0012',
+	'GEC0013',//社會
+	'GEC0014',
+	'GEC0017',//校基本
+	'GEC0018',
+	'GEC5098',//自然
+	'GEC4098',//群己
+	'GEC1098',//文化
+	'GEC3098' //公民
+]
