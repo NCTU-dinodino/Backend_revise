c244894d80b0df763cc2da0b7e9790698236d9ed
lspss9950101 <lspss9950101@gmail.com>
2020-06-11 15:52:04 +0800

    fix bugs

--- public/src/graduate/mergeDuplicates.js
+++ public/src/graduate/mergeDuplicates.js
@@ -4,7 +4,7 @@ function mergeDuplicate(req, res, next) {
 	req.csca.data.taken_courses.forEach((course) => {
 		if (merge_exceptions.some(code => course.code == code)){
 			let i = 1;
-			while(!req.csca.courses[course.code + '_' + i]) i++;
+			while(req.csca.courses[course.code + '_' + i]) i++;
 			req.csca.courses[course.code + '_' + i] = course;
 		} else if (req.csca.courses[course.code]) req.csca.courses[course.code].append(course);
 		else req.csca.courses[course.code] = course;
