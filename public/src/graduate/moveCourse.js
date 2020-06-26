1c2ef98da3a98c13380ca841ee5baf27ac7e4fe0
hungwei123 <hungwei123.cs06@g2.nctu.edu.tw>
2020-06-26 14:16:21 +0800

    fix moving courses to graduated

--- public/src/graduate/moveCourse.js
+++ public/src/graduate/moveCourse.js
@@ -10,10 +10,27 @@ function moveCourse(req, res, next) {
 	var cos_name = req.body.cn;
 	var origin_group = req.body.origin_group;
 	var target_group = req.body.target_group;
+	var pro_elective_credits = 0
 
 	if(target_group == '抵免研究所課程'){
+		req.csca.classes.pro_elective.courses.forEach((course) => {
+			var cos_data = course.getRepresentingData();
+			if(cos_data.cname != cos_name){
+				if(cos_data.reason == 'now'){
+					if(cos_data.cname.includes("物理")){
+						pro_elective_credits += 1;
+					}
+					else{
+						pro_elective_credits += course.original_credit;
+					}
+				}
+				else{
+					pro_elective_credits += course.real_credit;
+				}
+			}
+		});
 		req.csca.classes.pro_elective.calculateCredit();
-		if(req.csca.classes.pro_elective.credit <= req.csca.classes.pro_elective.require){
+		if(pro_elective_credits < req.csca.classes.pro_elective.require){
 			req.csca.moveCourse.success = false;
 			req.csca.moveCourse.reason = '超過畢業學分才可移動';
 			return next();
